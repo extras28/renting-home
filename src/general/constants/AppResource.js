8fb1e454faaedc60ba7faa86bef3dd71f76b455a
4880tvh <vht29102001@gmail.com>
2022-11-06 13:25:02 +0700

    sign in

--- src/general/constants/AppResource.js
+++ src/general/constants/AppResource.js
@@ -1,7 +1,14 @@
 
 const AppResource = {
     images: {
-
+        imgGoogleLogo: require('assets/images/icon_google.png'),
+        houseTemplateImage: require('assets/images/house_template.png'),
+        backgroundLogin : require('assets/images/background_login.png'),
+        popularMark: require('assets/images/popular_mark.png'),
+        vector : require('assets/images/vector.png'),
+        area: require('assets/images/area.png'),
+        list: require('assets/images/list.png'),
+        house:require('assets/images/house.png')
     },
     icons: {
 
