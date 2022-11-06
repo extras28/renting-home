08abeb9c942894e3c4442814e28affa204b14ae8
4880tvh <vht29102001@gmail.com>
2022-11-06 21:54:34 +0700

    test

--- src/features/Auth/SignInScreen/index.js
+++ src/features/Auth/SignInScreen/index.js
@@ -3,49 +3,105 @@ import PropTypes from 'prop-types';
 import './style.scss';
 import { Placeholder } from 'react-bootstrap';
 import AppResource from 'general/constants/AppResource';
+import BaseTextField from 'general/components/Form/BaseTextField';
+import { useFormik } from 'formik';
+import AppButton from 'general/components/AppButton';
+import Utils from 'general/utils/Utils';
+import ToastHelper from 'general/helpers/ToastHelpers';
+import * as Yup from 'yup';
 
 SignInScreen.propTypes = {
 
 };
 
+const sTag = '[SignInScreen]';
+
 function SignInScreen(props) {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: async (values) => {
+            const params = { ...values };
+            let inputPassword = params.password;
+            params.password = Utils.sha256(inputPassword);
+            try {
+                console.log(params);
+                ToastHelper.showSuccess('Đăng nhập thành công')
+                // const res = unwrapResult(await dispatch(thunkSignIn(params)));
+                // if (res) {
+                //     const displayName = UserHelper.getDisplayName(res.account);
+                //     ToastHelper.showSuccess(`Xin chào, ${displayName}`);
+                //     navigate(-1);
+                // }
+            } catch (error) {
+                console.log(`${sTag} loggin error: ${error.message}`);
+                // ToastHelper.showError('Đăng nhập không thành công');
+            }
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required('Bạn chưa nhập email').email('Email không hợp lệ'),
+            password: Yup.string().required('Bạn chưa nhập mật khẩu'),
+        }),
+    })
+
     return (
 
 
-        <div className="row g-0">
-            <div className="col-lg-6 h-100 lg6 gradient-custom-2">
-                <div className='h4 text-start mt-5 ms-5 mb-5 pb-5 pt-5 ps-3 '>
+
+        <div className='SignInScreen row min-vh-100 m-0 bg-white'>
+            {/* left*/}
+            <div className='col-12 col-lg-6 p-0 bg-white'>
+                {/* header */}
+                <div className='py-10 pl-10 border-bottom'>
                     <i style={{ fontSize: '20px', color: '#7065F0' }} className="fas fa-home-heart "></i>
-                    <span style={{ color: '#100A55' }} className='font-weight-boldest'>Home Renting</span>
+                    <span style={{ color: '#100A55', fontSize: '20px' }} className='font-weight-boldest'>Home Renting</span>
                 </div>
 
-                            <p style={{ color: '#000929' }} className='h1 pt-1 pb-1 ps-5 font-weight-boldest '>Welcome back</p>
-                            <p className='ps-5 font-size-lighter'>Welcome back! Please enter your details.</p>
-                        </div>
-                        <div className='mb-3 ms-5 me-5  '>
-                            <label className="form-label font-weight-bolder">Email address</label>
-                            <input type="email" className="inputform form-control" placeholder="hi@example.com"></input>
-                        </div>
-                        <div className='mb-3 ms-5 me-5 '>
-                            <label className="form-label font-weight-bolder">Password</label>
-                            <input type="password" className="inputform form-control" placeholder="Enter password"></input>
-                        </div>
-                        <div className='text-center'>
-                            <p style={{ color: '#7065F0' }} className='text-center'>Forgot Password ?</p>
-                        </div>
-                        <div className="d-grid gap-2 ps-0 pe-0 ms-5 me-5 ">
-                            <button style={{ backgroundColor: '#7065F0', padding: '10px' }} className="btn btn-primary" type="button">Login</button>
-                            <button style={{ borderColor: '#D6DDEB', backgroundColor: 'white', color: 'black' }} className="btn text-center" type="button"><img style={{ paddingRight: '6px' }} src={AppResource.images.imgGoogleLogo} />Continue with Google</button>
-                        </div>
-                        <div className='text-center pt-5 mt-3'>
-                            <span>Don't have an account ? </span>
-                            <a className="font-weight-bolder" href="" style={{ color: 'black' }}><u>Sign up for free</u></a>
-                        </div>
+                {/* form */}
+                <div className='d-flex align-items-center justify-content-center mt-20'>
+                    <form style={{width: '352px'}}  onClick={formik.handleSubmit}>
+                        <p style={{ color: '#000929', fontSize: '32px' }} className='font-weight-boldest '>Welcome back</p>
+                        <p className='font-weight-bold' style={{ color: '#000929', fontSize: '16px', opacity: '0.5' }}>Welcome back! Please enter your details.</p>
+                        <BaseTextField 
+                            name='email'
+                            label='Email'
+                            placeholder='Nhập email...'
+                            fieldProps={formik.getFieldProps('email')}
+                            fieldHelper={formik.getFieldHelpers('email')}
+                            fieldMeta={formik.getFieldMeta('email')}
+                        />
+                        <BaseTextField 
+                            type='password'
+                            name='password'
+                            label='Mật khẩu'
+                            placeholder='Nhập mật khẩu'
+                            fieldProps={formik.getFieldProps('password')}
+                            fieldHelper={formik.getFieldHelpers('password')}
+                            fieldMeta={formik.getFieldMeta('password')}
+                        />
+                        <p style={{ color: '#7065F0' }} className='text-center cursor-pointer'>Forgot Password ?</p>
+                        <AppButton 
+                            className='w-100 btn-purple'
+                            style={{height: '48px'}}
+                            text='Login'
+                        />
+                        <AppButton 
+                            beforIcon={(<img src={AppResource.images.imgGoogleLogo} alt="google logo" />)}
+                            className='btn-grey w-100 mt-6'
+                            style={{backgroundColor: '#FFFFFF', border: '1px solid #D6DDEB', height: '48px'}}
+                            text='Continue with Google'
+                        />
+                        <p className='text-center mt-10'>Don't have an account ? <a className="font-weight-bolder" href="#" style={{ color: 'black' }}><u>Sign up for free</u></a></p>
                      
                     </form>
                 </div>
             </div>
-            <div style={{ backgroundColor: '#F7F7FD' }} className="col-lg-6 lg6 gradient-custom">
+
+            {/* right */}
+            <div className='SignInScreen_Right col-6 p-0'>
             <div className='login_background bg-image'>
                     <div style={{ paddingTop: '48px' }} className='pt-5 ps-5 pe-5 pb-5 h-100'>
                         <div style={{ width: '30vw', minWidth: '20rem' }} className="card mt-24 ms-5 bg-transparent pt-5 border-0">
