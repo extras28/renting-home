6434ca1958c65a9d4290bfe519b5c858e04ceaca
4880tvh <vht29102001@gmail.com>
2022-11-14 14:34:34 +0700

    update header landing page 1

--- src/general/components/Header/HeaderLandingPage/index.js
+++ src/general/components/Header/HeaderLandingPage/index.js
@@ -22,28 +22,27 @@ function HeaderLandingPage(props) {
     
    
     return (
-        <div className="HeaderLandingPage">
-            <nav className="navbar navbar-expand-md ">
-                <div className="col container-fluid">
-                    <div className='p-4'>
-                        <img className="pe-2 pb-3 HeaderLandingPage_icon" src={AppResource.images.houseIcon} alt=""></img>
+        <div className="HeaderLandingPage py-4 px-6">
+            <nav className="navbar navbar-expand-md p-0">
+                <div className="col container-fluid p-0">
+                    <div className='d-flex flex-row justify-content-center align-items-center'>
+                        <img className="HeaderLandingPage_icon" src={AppResource.images.houseIcon} alt=""></img>
                         <span style={{ color: '#100A55', fontSize: '20px' }} className='font-weight-boldest HeaderLandingPage_headerText'>Home Renting</span>
                     </div>
                     <button  className=" navbar-toggler sticky-md-top "  data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i style={{ fontSize: '28px', color: 'black' }} onClick={toggleShowing}  className={`${showing ? "fas fa-equals" :"fas fa-times" }`}></i>
-                    </button>                 
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
-
-                        <ul className="navbar-nav  mb-2 mb-lg-0">
+                        <ul className="navbar-nav  m-0">
                             <li className="nav-item">
-                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ps-3 pe-3" aria-current="page" href="#">Rent</a>
+                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ms-2" aria-current="page" href="#">Rent</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ps-3 pe-3" href="#">Buy</a>
+                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ms-2" href="#">Buy</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ps-3 pe-3" href="#">Sell</a>
+                                <a className="nav-link font-weight-bolder HeaderLandingPage_item ms-2" href="#">Sell</a>
                             </li>                         
                                 <Dropdown>
                                     <Dropdown.Toggle className="font-weight-bolder border-0 HeaderLandingPage_dropdown" variant="" id="dropdown-basic">
@@ -72,10 +71,10 @@ function HeaderLandingPage(props) {
                                 </Dropdown>
                             
                         </ul>
-                        <div className=" w-100 float-end" >
+                        <div className="w-100 float-end" >
 
-                            <button style={{ minWidth: '80px', backgroundColor: '#7065f0' }} className='float-end btn btn-primary pt-3 pb-3 mt-5 mb-5 me-3'>Sign up</button>
-                            <button style={{ minWidth: '80px', color:'#7065f0',border: '2px solid #F9FAFB', borderRadius: '8px', backgroundColor: '#edecfb' }} className='HeaderLandingPage_left btn pt-3 pb-3 mt-5 mb-5 ms-2 me-2 font-weight-bolder'>Login</button>
+                            <button style={{backgroundColor: '#7065f0' }} className='float-end btn btn-primary '>Sign up</button>
+                            <button style={{color:'#7065f0',border: '2px solid #F9FAFB', borderRadius: '8px', backgroundColor: '#edecfb' }} className='HeaderLandingPage_left me-2 btn font-weight-bolder'>Login</button>
                         </div>
                     </div>
 
